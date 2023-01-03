27c0ffb4fa1bc8699220eed9c05e5554b9438fd3
ShamsutdinovAnvar <a.shamsutdinov2021@gmail.com>
2023-01-03 15:28:13 +0500

    search test is first

--- src/test/java/tests/AndroidTests.java
+++ src/test/java/tests/AndroidTests.java
@@ -4,12 +4,19 @@ import io.appium.java_client.AppiumBy;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 
 public class AndroidTests extends TestBase {
+    @SneakyThrows
+    @Test
+    void searchTest() {
+        $(AppiumBy.id("com.wegotrip.app:id/close")).click();
+        $(AppiumBy.accessibilityId("Search…")).click();
+        $(AppiumBy.id("com.wegotrip.app:id/search_src_text")).sendKeys("Berlin");
+        Thread.sleep(5000);
+        $(AppiumBy.id("com.wegotrip.app:id/title")).shouldHave(text("Berlin, Germany"));
+    }
 
     @Test
     void firstScreenTest() {
@@ -34,16 +41,6 @@ public class AndroidTests extends TestBase {
         Thread.sleep(2000);
         $(AppiumBy.id("com.wegotrip.app:id/title")).shouldHave(text("Audio tours work offline"));
 
-    }
-
-    @SneakyThrows
-    @Test
-    void searchTest() {
-        $(AppiumBy.id("com.wegotrip.app:id/close")).click();
-        $(AppiumBy.accessibilityId("Search…")).click();
-        $(AppiumBy.id("com.wegotrip.app:id/search_src_text")).sendKeys("Berlin");
-        Thread.sleep(5000);
-        $(AppiumBy.id("com.wegotrip.app:id/title")).shouldHave(text("Berlin, Germany"));
     }
 }
 
